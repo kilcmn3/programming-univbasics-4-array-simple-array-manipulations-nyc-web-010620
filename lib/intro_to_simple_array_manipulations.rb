7b5371d884d9828fcf6da822d62a273085444fc9
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-27 23:00:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,9 +26,10 @@ def using_concat(my_favorite_things, all_my_favs)
   my_favorite_things.concat(all_my_favs)
 end
 
-def using_concat(list_of_programming_languages)
+def using_insert(list_of_programming_languages)
   list_of_programming_languages.concat
 end
+
 
 def using_uniq(haircutsy)
   using_uniq
