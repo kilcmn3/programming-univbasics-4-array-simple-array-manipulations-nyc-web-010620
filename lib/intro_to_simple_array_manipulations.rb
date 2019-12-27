dafcd8eb184894e6b5a04418ab492818d75db841
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-27 22:25:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,9 +15,17 @@ def pop_with_args (dog_breeds, small_dogs)
 end
 
 def using_shift (my_favorite_cities, im_so_over_this_city)
-  my_favorite_cities(im_so_over_this_city)
+  my_favorite_cities.shift(im_so_over_this_city)
 end
 
 def shift_with_args(ice_cream_brands, brands_removed)
-  ice_cream_brands(brands_removed)
+  ice_cream_brands.shift(brands_removed)
+end
+
+def using_concat(my_favorite_things, all_my_favs)
+  my_favorite_things.concat(all_my_favs)
+end
+
+def using_concat(list_of_programming_languages, another_language, new_array)
+  list_of_programming_languages.concat(another_language, new_array)
 end
